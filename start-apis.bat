8f6c32d68d7c4233b77b80339ca5e5511267e43c
Marcos Diaz <ghostframez@gmail.com>
2021-01-22 00:16:55 -0300

    Refactoring

--- start-apis.bat
+++ start-apis.bat
@@ -1,3 +1,3 @@
-start mvn -f ../web-server/pom.xml spring-boot:run
-start mvn -f ../slow-api/pom.xml spring-boot:run
-start mvn -f ../webflux-server/pom.xml spring-boot:run
+start "Slow External API" mvn -f slow-api/pom.xml spring-boot:run
+start "Spring Web" mvn -f web-server/pom.xml spring-boot:run
+start "Spring Webflux" mvn -f webflux-server/pom.xml spring-boot:run
